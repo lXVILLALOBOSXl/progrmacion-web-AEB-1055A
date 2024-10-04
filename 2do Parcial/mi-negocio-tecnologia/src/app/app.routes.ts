536954ddc76f6713b174d0c48f4bfe53b20fb92b
Luis Villalobos <ivillalobosrivera@yahoo.com>
2024-10-04 12:15:41 -0600

    Mi_Negocio_Tecnologia

--- 2do Parcial/mi-negocio-tecnologia/src/app/app.routes.ts
+++ 2do Parcial/mi-negocio-tecnologia/src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { CatalogoComponent } from './components/catalogo/catalogo.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { ContactoComponent } from './components/contacto/contacto.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', component: InicioComponent },
-  { path: 'catalogo', component: CatalogoComponent },
-  { path: 'blog', component: BlogComponent },
-  { path: 'contacto', component: ContactoComponent },
   { path: '**', redirectTo: 'inicio' } // Redirige cualquier ruta no válida a 'inicio'
 ];
