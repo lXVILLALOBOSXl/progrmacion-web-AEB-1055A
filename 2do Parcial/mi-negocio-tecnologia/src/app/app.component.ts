a9f181dfe0403a128162eca4e377172c6ad68e6a
Luis Villalobos <ivillalobosrivera@yahoo.com>
2024-10-04 11:34:47 -0600

    Not in Single page Working

---
+++ 2do Parcial/mi-negocio-tecnologia/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+})
+export class AppComponent {}
---
+++ 2do Parcial/mi-negocio-tecnologia/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  imports: [RouterModule],  // Agregar RouterModule si usas routerLink en este componente.
+})
+export class NavbarComponent {}
